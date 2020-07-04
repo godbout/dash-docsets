71107e9802435716802d1bf3dbc2f9c26b90390a
Guill Lo <guill.bout@gmail.com>
2020-07-04 23:25:37 +0800

    finish alfred4 docset class

--- app/Docsets/Alfred4.php
+++ app/Docsets/Alfred4.php
@@ -5,6 +5,7 @@ namespace App\Docsets;
 use Godbout\DashDocsetBuilder\Docsets\BaseDocset;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Wa72\HtmlPageDom\HtmlPageCrawler;
 
 class Alfred4 extends BaseDocset
@@ -14,8 +15,8 @@ class Alfred4 extends BaseDocset
     public const URL = 'www.alfredapp.com';
     public const INDEX = 'help/index.html';
     public const PLAYGROUND = '';
-    public const ICON_16 = 'favicon-16x16.png';
-    public const ICON_32 = 'favicon-32x32.png';
+    public const ICON_16 = '../../icons/icon.png';
+    public const ICON_32 = '../../icons/icon@2x.png';
     public const EXTERNAL_DOMAINS = [
         'fonts.googleapis.com',
     ];
@@ -23,9 +24,6 @@ class Alfred4 extends BaseDocset
 
     public function grab(): bool
     {
-        $toIgnore = implode('|', [
-        ]);
-
         $toGet = implode('|', [
            '\.css',
             '\.ico',
@@ -61,11 +59,39 @@ class Alfred4 extends BaseDocset
     {
         $crawler = HtmlPageCrawler::create(Storage::get($file));
 
-        $crawler->filter('h1')->each(function (HtmlPageCrawler $node) use ($entries) {
+        $entries = collect();
+        $entries = $entries->merge($this->guideEntries($crawler, $file));
+        $entries = $entries->merge($this->sectionEntries($crawler, $file));
+
+        return $entries;
+    }
+
+    protected function guideEntries(HtmlPageCrawler $crawler, string $file)
+    {
+        $entries = collect();
+
+        if (Str::contains($file, $this->index())) {
+            $crawler->filter('#helpmenu a')->each(function (HtmlPageCrawler $node) use ($entries) {
+                $entries->push([
+                    'name' => trim($node->text()),
+                    'type' => 'Guide',
+                    'path' => $this->url() . '/help/' . $node->attr('href'),
+                ]);
+            });
+        }
+
+        return $entries;
+    }
+
+    protected function sectionEntries(HtmlPageCrawler $crawler, string $file)
+    {
+        $entries = collect();
+
+        $crawler->filter('h1:not(:first-child), h2')->each(function (HtmlPageCrawler $node) use ($entries, $file) {
             $entries->push([
-                'name' => 'What Shows up in Dash',
-                'type' => 'One of the Million of Dash Entry Type',
-                'path' => 'The Path to the File',
+                'name' => trim($node->text()),
+                'type' => 'Section',
+                'path' => Str::after($file, $this->innerDirectory())
             ]);
         });
 
@@ -76,11 +102,72 @@ class Alfred4 extends BaseDocset
     {
         $crawler = HtmlPageCrawler::create(Storage::get($file));
 
-        /**
-         * This is equivalent to murder
-         */
-        $crawler->filter('body')->remove();
+        $this->removeMainNav($crawler);
+        $this->removeSubNav($crawler);
+        $this->removeHelpMenu($crawler);
+        $this->removeLatestBlogPostBanner($crawler);
+        $this->removeFooter($crawler);
+        $this->removeFooterMeta($crawler);
+
+        $this->removeUnwantedJavaScript($crawler);
+
+        $this->insertOnlineRedirection($crawler, $file);
+        $this->insertDashTableOfContents($crawler);
 
         return $crawler->saveHTML();
+    }
+
+    protected function removeMainNav(HtmlPageCrawler $crawler)
+    {
+        $crawler->filter('#mainnav')->remove();
+    }
+
+    protected function removeSubNav(HtmlPageCrawler $crawler)
+    {
+        $crawler->filter('#subnav')->remove();
+    }
+
+    protected function removeHelpMenu(HtmlPageCrawler $crawler)
+    {
+        $crawler->filter('#helpmenu')->remove();
+    }
+
+    protected function removeLatestBlogPostBanner(HtmlPageCrawler $crawler)
+    {
+        $crawler->filter('#latestblogpost')->remove();
+    }
+
+    protected function removeFooter(HtmlPageCrawler $crawler)
+    {
+        $crawler->filter('#footer')->remove();
+    }
+
+    protected function removeFooterMeta(HtmlPageCrawler $crawler)
+    {
+        $crawler->filter('#footermeta')->remove();
+    }
+
+    protected function removeUnwantedJavaScript(HtmlPageCrawler $crawler)
+    {
+        $crawler->filterXPath("//script[text()[contains(.,'google-analytics.com')]]")->remove();
+    }
+
+    protected function insertOnlineRedirection(HtmlPageCrawler $crawler, string $file)
+    {
+        $onlineUrl = Str::substr(Str::after($file, $this->innerDirectory()), 1, -11);
+
+        $crawler->filter('html')->prepend("<!-- Online page at $onlineUrl -->");
+    }
+
+    protected function insertDashTableOfContents($crawler)
+    {
+        $crawler->filter('head')
+            ->before('<a name="//apple_ref/cpp/Section/Top" class="dashAnchor"></a>');
+
+        $crawler->filter('h1:not(:first-child), h2, h3')->each(static function (HtmlPageCrawler $node) {
+            $node->before(
+                '<a id="' . Str::slug($node->text()) . '" name="//apple_ref/cpp/Section/' . rawurlencode($node->text()) . '" class="dashAnchor"></a>'
+            );
+        });
     }
 }
